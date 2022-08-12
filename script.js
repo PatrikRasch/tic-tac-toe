98423a6b6716d90b9ba9457b9d6f0e1ac162d38b
PatrikRasch <patrikrask@hotmail.com>
2022-08-12 12:33:40 +0200

    Gameboard has active eventlisteners and is generated using for loop within a module. Clicking currently only logs which square has been clicked.

--- script.js
+++ script.js
@@ -1,49 +1,94 @@
 const gameboard = (function () {
     let playingField = [
-        ["X", "X", "O"],
-        ["O", "O", "O"],
-        ["X", "O", "X"]
+        "", "", "", "", "", "", "", "", ""
     ];
-    return {
-        playingField
-    }
+    return { playingField }
 })();
 
+const plays = gameboard.playingField;
 
+// let bro = plays.map(function (_, i) {
+//     let el = document.createElement("div");
+//     el.textContent = plays.splice(i, 1, i + 1);
+//     return el;
+// })
+// document.body.append(bro.el)
+
+// let el = document.createElement("div");
+// el.textContent = plays.splice(i, 1, i + 1);
+// console.log(el.textContent);
+// return el;
+
+const gameboardContainer = document.querySelector(".gameboard-container");
+const board = document.querySelector(".gameboard");
+// gameboardContainer.append(board)
 
 const renderGameboard = (function () {
-    publicMethod = function (testie) {
-        const gameboard = document.querySelector(".gameboard");
-        for (i = 1; i <= 9; i++) {
-            const spot = document.createElement("div");
-            spot.id = i;
-            spot.textContent = spot.id
-            gameboard.append(spot);
+    function addElements() {
+        for (i = 0; i < plays.length; i++) {
+            const temp = document.createElement("div");
+            temp.textContent = ""
+            // Math.random() < 0.5 ? "X" : "O";
+            plays[i] = temp.textContent;
+            temp.id = ("id-text" + plays[i] + (i + 1));
+            board.appendChild(temp);
+            // temp.classList.add("gameboard");
+            temp.classList.add("spot");
+            temp.addEventListener("click", (e) => {
+                console.log(temp.id)
+            })
         }
-        // const spot1 = gameboard.playingField[0][0];
-        // const spot2 = gameboard.playingField[0][1];
-        // const spot3 = gameboard.playingField[0][2];
-        // const spot4 = gameboard.playingField[1][0];
-        // const spot5 = gameboard.playingField[1][1];
-        // const spot6 = gameboard.playingField[1][2];
-        // const spot7 = gameboard.playingField[2][0];
-        // const spot8 = gameboard.playingField[2][1];
-        //     // const spot9 = gameboard.playingField[2][2];
-
-        //     document.createElement(spot1, spot2, spot3);
-        //     document.createElement(spot4, spot5, spot6);
-        //     document.createElement(spot7, spot8, spot9);
-
-        //     document.body.append(spot1, spot2, spot3);
-        //     document.body.append(spot4, spot5, spot6);
-        //     document.body.append(spot7, spot8, spot9);
+        console.log(plays);
+        // return numberedArray
     }
-    return { publicMethod }
-}
-)();
+    return { addElements: addElements, }
+})();
 
-const testie = renderGameboard.publicMethod("dug");
-// classAdd.classList.add("bruh")
+const renderedGameboard = renderGameboard.addElements();
+// console.log(renderGameboard.addElements);
+// const gameboard = document.querySelector(".gameboard");
+// for (i = 1; i <= 9; i++) {
+// const spot = document.createElement("div");
+// spot.id = i;
+// const spotInfo = spot.textContent = Math.random() < 0.5 ? "X" : "O";
+// console.log(spotInfo)
+// gameboard.append(spot);
+// spot.classList.add("spot")
+
+
+// for (var i = 0; i < imageArray.length; i++) {
+//     (function(index) {
+//          imageArray[index].addEventListener("click", function() {
+//             console.log("you clicked region number " + index);
+//           })
+//     })(i);
+//  }
+
+
+// document.body.appendChild(document.createElement("div"))
+
+// const renderGameboard = (function () {
+//     render = function () {
+//         const gameboard = document.querySelector(".gameboard");
+//         for (i = 1; i <= 9; i++) {
+//             const spot = document.createElement("div");
+//             spot.id = i;
+//             const spotInfo = spot.textContent = Math.random() < 0.5 ? "X" : "O";
+//             // console.log(spotInfo)
+//             gameboard.append(spot);
+//             spot.classList.add("spot")
+//         }
+//     }
+//     return { render }
+// }
+// )();
+// renderGameboard.render();
+
+const gameboardClicked = (function () {
+    clicked = function () {
+
+    }
+})();
 
 const playerFactory = (name, number) => {
     return {
@@ -55,17 +100,11 @@ const playerFactory = (name, number) => {
     }
 };
 
-
-
 const player1 = playerFactory("Patrik", 1);
 player1.greetPlayer();
 
 const player2 = playerFactory("Annabelle", 2);
 player2.greetPlayer();
-
-// const gameplay = {
-
-// }
 
 
 // 2. You're going to store the gameboard as an array inside of a Gameboard object, so start there! Your players are also going to be stored in objects. and you're probably going to want an object to control the flow of the game itself.
@@ -77,7 +116,6 @@ player2.greetPlayer();
 // 4. Build the functions that allow players to add marks to a specific spot on the board, and then tie it to the DOM, letting players click on the gameboard to place their marker. Don’t forget the logic that keeps players from playing in spots that are already taken!
 
 // 4. a)Think carefully about where each bit of logic should reside. Each little piece of functionality should be able to fit in the game, player or gameboard objects.. but take care to put them in “logical” places. Spending a little time brainstorming here can make your life much easier later!
-
 
 
 // Factory functions are fucking cool compared to regular objects because users can't just
