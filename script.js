621a6f5ef5bdb70cfb51f496fef4b8f515ae5fd0
PatrikRasch <patrikrask@hotmail.com>
2022-08-27 12:07:30 +0200

    Game now uses x's and o's upon click and further checks for wins/losses. Uses a modal popup to alert the players of the winner. Game does not check for ties yet.

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ const plays = gameboard.playingField;
 const gameboardContainer = document.querySelector(".gameboard-container");
 const board = document.querySelector(".gameboard");
 
+const modalbg = document.querySelector(".modal-bg")
+const modal = document.querySelector(".modal")
+
+const acceptLoss = document.querySelector(".acceptLoss")
+const negotiate = document.querySelector(".negotiate")
+
 const renderGameboard = (function () {
     function addElements() {
         for (i = 0; i < plays.length; i++) {
@@ -27,8 +33,18 @@ const renderGameboard = (function () {
 const game = (function () {
     function boardClicked() {
         let yourTurn = true;
+
+        const cardClicked = () => {
+            for(i=0; i<9; i++) {
+                board.children[i].classList.remove("x")
+                board.children[i].classList.remove("o")
+                board.children[i].classList.remove("disabled")
+            }
+            modalbg.classList.remove("bg-active")
+            modal.classList.remove("modal-active")
+        }
+
         board.addEventListener("click", (e) => {
-            // console.log(e.target.id)
             if (e.target.classList.contains("disabled")) { return }
             if (yourTurn == true) {
                 e.target.classList.add("x")
@@ -39,22 +55,42 @@ const game = (function () {
                 e.target.classList.add("disabled")
                 yourTurn = true;
             }
+
+            winTest = (a,b,c, player) => {
+            if(board.children[a].classList.contains(player) && board.children[b].classList.contains(player) && board.children[c].classList.contains(player) )
+              {  
+                console.log(player + " wins")
+                modalbg.classList.add("bg-active")
+                modal.classList.add("modal-active")
+
+                acceptLoss.addEventListener("click", (e) => {
+                    cardClicked();
+                })
+                negotiate.addEventListener("click", (e) => {
+                    alert("sad")
+                    cardClicked();
+                })
+            } }
+
+            winTest(0,1,2, "x")
+            winTest(3,4,5, "x")
+            winTest(6,7,8, "x")
+            winTest(0,3,6, "x")
+            winTest(1,4,7, "x")
+            winTest(2,5,8, "x")
+            winTest(0,4,8, "x")
+            winTest(2,4,6, "x")
+
+            winTest(0,1,2, "o")
+            winTest(3,4,5, "o")
+            winTest(6,7,8, "o")
+            winTest(0,3,6, "o")
+            winTest(1,4,7, "o")
+            winTest(2,5,8, "o")
+            winTest(0,4,8, "o")
+            winTest(2,4,6, "o")
         })
     }
-    xState: [];
-    oState: [];
-    winningStates: [
-        ['0', '1', '2'],
-        ['3', '4', '5'],
-        ['6', '7', '8'],
-
-        ['0', '4', '8'],
-        ['2', '4', '6'],
-
-        ['0', '3', '6'],
-        ['1', '4', '7'],
-        ['2', '5', '8'],
-    ]
     return { boardClicked: boardClicked }
 })();
 
@@ -80,31 +116,13 @@ const playerClicks = (player1, player2) => {
 
 }
 
-// const spotClicked = renderGameboard.addElements;
-
-// const playersClick = (function () {
-//     function clickSpot() {
-
-//     }
-// })();
-
-// let bro = plays.map(function (_, i) {
-//     let el = document.createElement("div");
-//     el.textContent = plays.splice(i, 1, i + 1);
-//     return el;
-// })
-// document.body.append(bro.el)
-
-// let el = document.createElement("div");
-// el.textContent = plays.splice(i, 1, i + 1);
-// console.log(el.textContent);
-// return el;
-
-
-
-
-
 const renderedGameboard = renderGameboard.addElements();
+
+
+
+
+
+
 // console.log(renderGameboard.addElements);
 // const gameboard = document.querySelector(".gameboard");
 // for (i = 1; i <= 9; i++) {
@@ -114,16 +132,6 @@ const renderedGameboard = renderGameboard.addElements();
 // console.log(spotInfo)
 // gameboard.append(spot);
 // spot.classList.add("spot")
-
-
-// for (var i = 0; i < imageArray.length; i++) {
-//     (function(index) {
-//          imageArray[index].addEventListener("click", function() {
-//             console.log("you clicked region number " + index);
-//           })
-//     })(i);
-//  }
-
 
 // document.body.appendChild(document.createElement("div"))
 
@@ -144,19 +152,5 @@ const renderedGameboard = renderGameboard.addElements();
 // )();
 // renderGameboard.render();
 
-
-
-// 2. You're going to store the gameboard as an array inside of a Gameboard object, so start there! Your players are also going to be stored in objects. and you're probably going to want an object to control the flow of the game itself.
-
-// 2. a) Your main goal here is to have as little global code as possible. Try tucking everything away inside of a module or factory. Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories.
-
-// 3. Set up your HTML and write a JavaScript function that will render the contents of the gameboard array to the webpage(for now you can just manually fill in the array with "X"s and "O"s)
-
-// 4. Build the functions that allow players to add marks to a specific spot on the board, and then tie it to the DOM, letting players click on the gameboard to place their marker. Don’t forget the logic that keeps players from playing in spots that are already taken!
-
-// 4. a)Think carefully about where each bit of logic should reside. Each little piece of functionality should be able to fit in the game, player or gameboard objects.. but take care to put them in “logical” places. Spending a little time brainstorming here can make your life much easier later!
-
-
 // Factory functions are fucking cool compared to regular objects because users can't just
-// overwrite any of the object items when they want.
-
+// overwrite any of the object items if they want to.
